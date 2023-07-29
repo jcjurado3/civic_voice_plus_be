643b8ea718b03bde24a3f5ba93547ae21a84117c
Ashley Plasket <ashley.plasket@gmail.com>
2023-07-29 18:01:59 -0400

    add bill controller route & search action
    Co-authored-by: Jeffrey Redish <Jredish11@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  namespace :api do
+    namespace :v1 do
+      get "/bills", to: "bills#search"
+    end
+  end
 end
