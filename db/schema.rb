d27d6353161b2a93a25b71b96b00d7b4e7c22358
Ashley Plasket <ashley.plasket@gmail.com>
2023-07-28 21:05:02 -0400

    add user state model and associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_29_005855) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_29_010157) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,5 +35,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_29_005855) do
     t.index ["user_id"], name: "index_user_categories_on_user_id"
   end
 
+  create_table "user_states", force: :cascade do |t|
+    t.bigint "state_id", null: false
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["state_id"], name: "index_user_states_on_state_id"
+    t.index ["user_id"], name: "index_user_states_on_user_id"
+  end
+
   add_foreign_key "user_categories", "categories"
+  add_foreign_key "user_states", "states"
 end
--- spec/models/state_spec.rb
+++ spec/models/state_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe State, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "relationships" do
+    it { should have_many :user_states }
+  end
 end
