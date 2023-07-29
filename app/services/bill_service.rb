0be44a692c268efe9d29f15f07d657c0adaa3662
Ashley Plasket <ashley.plasket@gmail.com>
2023-07-29 18:03:14 -0400

    add bill service api connections for endpoints

---
+++ app/services/bill_service.rb
@@ -0,0 +1,32 @@
+class BillService
+  def initialize(params)
+    @ops_action = params[:op]
+    @state_abbv = params[:state]
+    @query_keyword = params[:query]
+  end
+
+  def bills_by_query
+    response = conn.get('?') do |request|
+      request.params['key'] = ENV["LEGISCAN_KEY"]
+      request.params['op'] = @ops_action
+      request.params['state'] = @state_abbv
+      request.params['query'] = @query_keyword
+    end
+
+    json = JSON.parse(response.body, symbolize_names: true)
+  end
+
+  # def bills_by_id()
+  #   response = conn.get('?') do |request|
+  #     request.params['key'] = ENV["LEGISCAN_KEY"]
+  #     request.params['op'] = "#{ops_param}"
+  #     request.params['id'] = "#{bill_id}"
+  #   end
+
+  #   json = JSON.parse(response.body, symbolize_names: true)
+  # end
+
+  def conn
+    Faraday.new("https://api.legiscan.com/")
+  end
+end
