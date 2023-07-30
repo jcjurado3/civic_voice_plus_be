6723df6533a6ba444803be88a77942f9dd0ed7fb
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-07-30 09:13:58 -0600

    hard code op with getSearch for bill_by_query method

--- app/services/bill_service.rb
+++ app/services/bill_service.rb
@@ -1,6 +1,5 @@
  class BillService
   def initialize(params)
-    @ops_action = params[:op]
     @state_abbv = params[:state]
     @query_keyword = params[:query]
     @bill_id = params[:id].to_i
@@ -9,7 +8,7 @@
   def bills_by_query
     response = conn.get('?') do |request|
       request.params['key'] = ENV["LEGISCAN_KEY"]
-      request.params['op'] = @ops_action
+      request.params['op'] = "getSearch"
       request.params['state'] = @state_abbv
       request.params['query'] = @query_keyword
     end
