84b2b3d3fdba267769354417087d4b82c85ab0ac
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-07-29 21:28:07 -0600

    add tests for poros, need to fix

--- spec/poros/api_bill_spec.rb
+++ spec/poros/api_bill_spec.rb
@@ -21,5 +21,22 @@ RSpec.describe ApiBill do
       expect(bills_data.first.text_url).to be_a(String)
       expect(bills_data.first.title).to be_a(String)
     end
+
+    it "has more attributes" do
+      params = {
+              op: "getBill",
+              id: 1722281
+              }
+      
+      bills_data = BillSearchFacade.new(params).bill
+        expect(bills_data.status).to be_a(Integer)
+        expect(bills_data.description).to be_a(String)
+        expect(bills_data.sponsors).to be_a(Array)
+
+      bills_data.sponsors.map do |sponsor|
+        expect(sponsor[:name]).to be_a(String)
+        expect(sponsor[:party_id]).to be_a(String)
+      end
+    end
   end
 end
