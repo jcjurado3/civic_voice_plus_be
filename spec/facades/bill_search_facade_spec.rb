27495fadfd7fe06ad6a64e84f9b8f054e8a6d56d
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-07-29 21:28:53 -0600

    add in test setup for bill method

--- spec/facades/bill_search_facade_spec.rb
+++ spec/facades/bill_search_facade_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe BillSearchFacade do
           query: "healthcare" }
 
         bills = BillSearchFacade.new(params).bills
-
+        
         expect(bills).to be_an(Array)
         expect(bills.first).to be_a(ApiBill)
         expect(bills.first.bill_id).to be_an(Integer)
@@ -21,6 +21,17 @@ RSpec.describe BillSearchFacade do
         expect(bills.first.state).to be_a(String)
         expect(bills.first.text_url).to be_a(String)
         expect(bills.first.title).to be_a(String)
+      end
+    end
+    
+    describe "#bill" do
+      it "returns a single bill based of bill_id number" do
+        params = {
+          op: "getBill",
+          id: 1722281
+        }
+        
+        bill = BillSearchFacade.new(params).bill
       end
     end
   end
