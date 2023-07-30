09ea8c660921363a9ce1253afe8f283a8ec600a0
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-07-30 09:46:18 -0600

    fix some expects, add tests for doc_id

--- spec/poros/api_bill_spec.rb
+++ spec/poros/api_bill_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe ApiBill do
   describe "existance" do
     it "exists and has attributes", :vcr do
       params = {
-          op: "getSearch",
           state: "FL",
           query: "healthcare" }
 
@@ -20,11 +19,14 @@ RSpec.describe ApiBill do
       expect(bills_data.first.state).to be_a(String)
       expect(bills_data.first.text_url).to be_a(String)
       expect(bills_data.first.title).to be_a(String)
+      expect(bills_data.first.status).to be_a(NilClass)
+      expect(bills_data.first.description).to be_a(NilClass)
+      expect(bills_data.first.sponsors).to be_a(NilClass)
+      expect(bills_data.first.text).to be_a(NilClass)
     end
 
     it "has more attributes" do
       params = {
-              op: "getBill",
               id: 1722281
               }
       
@@ -32,7 +34,15 @@ RSpec.describe ApiBill do
         expect(bills_data.status).to be_a(Integer)
         expect(bills_data.description).to be_a(String)
         expect(bills_data.sponsors).to be_a(Array)
-
+        expect(bills_data.text_url).to be_a(NilClass)
+        expect(bills_data.last_action).to be_a(NilClass)
+        expect(bills_data.last_action_date).to be_a(NilClass)
+        expect(bills_data.text).to be_an(Array)
+        
+      bills_data.text.map do |doc|
+        expect(doc[:doc_id]).to be_a(Integer)
+      end
+      
       bills_data.sponsors.map do |sponsor|
         expect(sponsor[:name]).to be_a(String)
         expect(sponsor[:party_id]).to be_a(String)
