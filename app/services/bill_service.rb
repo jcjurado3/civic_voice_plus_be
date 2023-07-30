b4c29b4ea52aecf89a027d2202dce98b03cae211
Ashley Plasket <ashley.plasket@gmail.com>
2023-07-29 21:22:13 -0400

    add service for bill by id
    Co-authored-by: Jeffrey Redish <Jredish11@users.noreply.github.com>

--- app/services/bill_service.rb
+++ app/services/bill_service.rb
@@ -3,6 +3,7 @@ class BillService
     @ops_action = params[:op]
     @state_abbv = params[:state]
     @query_keyword = params[:query]
+    @bill_id = params[:id].to_i
   end
 
   def bills_by_query
@@ -16,15 +17,15 @@ class BillService
     json = JSON.parse(response.body, symbolize_names: true)
   end
 
-  # def bills_by_id()
-  #   response = conn.get('?') do |request|
-  #     request.params['key'] = ENV["LEGISCAN_KEY"]
-  #     request.params['op'] = "#{ops_param}"
-  #     request.params['id'] = "#{bill_id}"
-  #   end
+  def bills_by_id
+    response = conn.get('?') do |request|
+      request.params['key'] = ENV["LEGISCAN_KEY"]
+      request.params['op'] = "getBill"
+      request.params['id'] = @bill_id
+    end
 
-  #   json = JSON.parse(response.body, symbolize_names: true)
-  # end
+    json = JSON.parse(response.body, symbolize_names: true)
+  end
 
   def conn
     Faraday.new("https://api.legiscan.com/")
