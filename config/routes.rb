4cc67c28e5bd988b2451cca0a593aa69d5575ebc
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-07-30 12:14:47 -0600

    fix routes based on action refactor

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      get "/bills", to: "bills#search"
+      get "/bills", to: "bills#index"
       get "/bills/:id", to: "bills#show"
     end
   end
