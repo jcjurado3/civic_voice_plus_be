62fc748626feb565c3fad2acf1bde65a3cc15dd7
Ashley Plasket <ashley.plasket@gmail.com>
2023-07-29 21:21:13 -0400

    add bill method to facade
    Co-authored-by: Jeffrey Redish <Jredish11@users.noreply.github.com>

--- app/facades/bill_search_facade.rb
+++ app/facades/bill_search_facade.rb
@@ -6,7 +6,7 @@ class BillSearchFacade
   def bills
     bill_search = BillService.new(@params).bills_by_query
     bill_objects = []
-    bill_search[:searchresult].drop(1).to_h.map do |bill_data|
+    bill_search[:searchresult].drop(1).map do |bill_data|
       bill_hash = {id: bill_data.first.to_s.to_i,
       attributes: bill_data.last }
 
@@ -14,5 +14,13 @@ class BillSearchFacade
     end
 
     bill_objects
+  end
+
+  def bill
+    bill_search = BillService.new(@params).bills_by_id
+    bill_hash = { id: bill_search[:bill][:bill_id],
+      attributes: bill_search[:bill]
+    }
+    ApiBill.new(bill_hash)
   end
 end
