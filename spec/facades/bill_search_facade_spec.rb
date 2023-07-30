fe7eeaa6046f1ea29726f5ad3e709f37ccf10835
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-07-30 10:24:56 -0600

    add tests for get bills_by_id

--- spec/facades/bill_search_facade_spec.rb
+++ spec/facades/bill_search_facade_spec.rb
@@ -27,11 +27,29 @@ RSpec.describe BillSearchFacade do
     describe "#bill" do
       it "returns a single bill based of bill_id number", :vcr do
         params = {
-          op: "getBill",
           id: 1722281
         }
         
         bill = BillSearchFacade.new(params).bill
+        
+        expect(bill).to be_a(ApiBill)
+        expect(bill.bill_id).to be_an(Integer)
+        expect(bill.bill_id).to eq(1722281)
+        expect(bill.id).to be_an(Integer)
+        expect(bill.id).to eq(1722281)
+        expect(bill.bill_number).to be_a(String)
+        expect(bill.bill_number).to eq("H1255")
+        expect(bill.state).to be_a(String)
+        expect(bill.state).to eq("FL")
+        expect(bill.description).to be_a(String)
+        expect(bill.description).to eq("Abolishes district, transfers assets & liabilities of district; requires certain books to be deposited into Winter Garden Heritage Museum; provides effective date.")
+        expect(bill.status).to be_a(Integer)
+        expect(bill.sponsors).to be_a(Array)
+        expect(bill.sponsors.first[:name]).to eq("Doug Bankson")
+        expect(bill.text).to be_a(Array)
+        expect(bill.text_url).to be_a(NilClass)
+        expect(bill.last_action).to be_a(NilClass)
+        expect(bill.last_action_date).to be_a(NilClass)
       end
     end
   end
