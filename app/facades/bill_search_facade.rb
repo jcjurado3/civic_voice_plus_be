107bd87fbdb7e3290a2d88b2315bad814a8ef0dc
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-08-01 14:27:02 -0600

    add create bill to methods

--- app/facades/bill_search_facade.rb
+++ app/facades/bill_search_facade.rb
@@ -11,34 +11,52 @@ class BillSearchFacade
         attributes: bill_data.last }
 
         bill_objects << ApiBill.new(bill_hash)
-      end
-
-      bill_objects
+        create_bill(bill_data.last)
     end
+      bill_objects
+  end
 
-    def bill
-      bill_search = BillService.new(@params).bills_by_id
-      bill_hash = { id: bill_search[:bill][:bill_id],
-        attributes: bill_search[:bill]
-      }
-      ApiBill.new(bill_hash)
+  def bill
+    bill_search = BillService.new(@params).bills_by_id
+    bill_hash = { id: bill_search[:bill][:bill_id],
+      attributes: bill_search[:bill]
+    }
+    bill_object = ApiBill.new(bill_hash)
+    create_bill(bill_search[:bill])
+    bill_object
+  end
 
-#       bill_search = BillService.new(@params).bills_by_id
+  private
+  def create_bill(bill_data)
+    Bill.create!(
+      bill_id: bill_data[:bill_id],
+      bill_number: bill_data[:bill_number],
+      text_url: bill_data[:text_url],
+      last_action_date: bill_data[:last_action_date],
+      last_action: bill_data[:last_action],
+      title: bill_data[:title],
+      state: bill_data[:state]
+    )
+  end
 
-#       state_abbv = bill_search[:bill][:state]
-#       full_state = STATE_ABBR_TO_NAME[state_abbv]
+  def check_if_bill
+  end
+end
 
-#         sponsor_array = bill_search[:bill][:sponsors].map do |sponsor|
-#           sponsor[:name]
-#         end
+      # bill_search = BillService.new(@params).bills_by_id
+
+      # state_abbv = bill_search[:bill][:state]
+      # full_state = STATE_ABBR_TO_NAME[state_abbv]
+
+      #   sponsor_array = bill_search[:bill][:sponsors].map do |sponsor|
+      #     sponsor[:name]
+      #   end
 #   # if
 # #     rep = Representative.find_by(name)
 # #     else
 # #       BillSearchFacade.rep_details
 # #       rep.create
 #         rep_search = BillService.new(@params).rep_details(full_state, sponsor_array)
-    end
-  end
 
 
 
