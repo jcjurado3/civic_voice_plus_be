a90ee05237812882a2c4ec2ead5fb33e12bec8de
Ashley Plasket <ashley.plasket@gmail.com>
2023-08-02 13:12:39 -0400

    update to find userstate by user only and update the state if found

--- app/controllers/api/v1/user_states_controller.rb
+++ app/controllers/api/v1/user_states_controller.rb
@@ -5,11 +5,11 @@ class Api::V1::UserStatesController < ApplicationController
   end
 
   def create
-    u_state = UserState.find_by(user_id: params[:user_id], state_id: params[:state_id])
+    u_state = UserState.find_by(user_id: params[:user_id])
 
     if u_state
       u_state.update!(user_id: params[:user_id], state_id: params[:state_id])
-      render json: UserStateSerializer.new(u_state)
+      render json: UserStateSerializer.new(u_state), status: 200
     else
       new_user_state = UserState.create(user_id: params[:user_id], state_id: params[:state_id])
       render json: UserStateSerializer.new(new_user_state), status: 201
