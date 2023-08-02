7d2128a5d3fca26ff0c5f8d0c6ddd9cfd1977cc1
Ashley Plasket <ashley.plasket@gmail.com>
2023-08-02 13:13:22 -0400

    add test for updating userstate instead of creating if userstate association is found by user since the user can only have one state association

--- spec/requests/api/v1/user_states/user_states_reqest_spec.rb
+++ spec/requests/api/v1/user_states/user_states_reqest_spec.rb
@@ -67,5 +67,39 @@ RSpec.describe "User_State Endpoints" do
       expect(created_user_state[:data][:attributes]).to have_key(:state_id)
       expect(created_user_state[:data][:attributes][:state_id]).to be_an(Integer)
     end
+
+    it "updates the user_state if an association is already found" do
+        user1_id = "1"
+        state1 = create(:state)
+        user_state1 = UserState.create!(user_id: user1_id, state_id: state1.id )
+
+        state2 = create(:state)
+
+        headers = { 'CONTENT_TYPE' => 'application/json' }
+
+        post "/api/v1/user_states?user_id=#{user1_id}&state_id=#{state2.id}"
+
+        expect(response).to be_successful
+        expect(response.status).to eq(200)
+        updated_user_state = JSON.parse(response.body, symbolize_names: true)
+
+        expect(updated_user_state).to be_a Hash
+        expect(updated_user_state).to have_key(:data)
+        expect(updated_user_state[:data]).to have_key(:id)
+        expect(updated_user_state[:data][:id]).to be_a(String)
+
+        expect(updated_user_state[:data]).to have_key(:type)
+        expect(updated_user_state[:data][:type]).to be_a(String)
+        expect(updated_user_state[:data][:type]).to eq("user_state")
+
+        expect(updated_user_state[:data]).to have_key(:attributes)
+        expect(updated_user_state[:data][:attributes]).to have_key(:user_id)
+        expect(updated_user_state[:data][:attributes][:user_id]).to be_an(Integer)
+
+        expect(updated_user_state[:data][:attributes]).to have_key(:state_id)
+        expect(updated_user_state[:data][:attributes][:state_id]).to be_an(Integer)
+        expect(updated_user_state[:data][:attributes][:state_id]).to eq(state2.id)
+        expect(updated_user_state[:data][:attributes][:state_id]).to_not eq(state1.id)
+    end
   end
 end
