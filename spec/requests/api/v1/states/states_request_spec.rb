d5f444e02fd631e6c0af5c6bda5c2a263dd5e170
Ashley Plasket <ashley.plasket@gmail.com>
2023-08-02 12:30:28 -0400

    add serializer for states and finish index request

--- spec/requests/api/v1/states/states_request_spec.rb
+++ spec/requests/api/v1/states/states_request_spec.rb
@@ -12,7 +12,27 @@ RSpec.describe "States Endpoints" do
 
       state_data = JSON.parse(response.body, symbolize_names: true)
 
-      binding.pry
+      expect(state_data).to have_key(:data)
+      expect(state_data[:data]).to be_an(Array)
+      expect(state_data[:data].count).to eq(10)
+
+      state_data[:data].map do |state|
+        expect(state).to have_key(:id)
+        expect(state[:id]).to be_a(String)
+
+        expect(state).to have_key(:type)
+        expect(state[:type]).to be_a(String)
+        expect(state[:type]).to eq("state")
+
+        expect(state).to have_key(:attributes)
+        expect(state[:attributes]).to be_a(Hash)
+
+        expect(state[:attributes]).to have_key(:state_abbr)
+        expect(state[:attributes][:state_abbr]).to be_a(String)
+
+        expect(state[:attributes]).to have_key(:state_name)
+        expect(state[:attributes][:state_name]).to be_a(String)
+      end
     end
   end
 end
