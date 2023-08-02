2c6a37c2beb47bb7a4135488b9db0f4ac0bcc810
Ashley Plasket <ashley.plasket@gmail.com>
2023-08-02 12:52:23 -0400

    add functionality for user_state index and create

---
+++ app/controllers/api/v1/user_states_controller.rb
@@ -0,0 +1,18 @@
+class Api::V1::UserStatesController < ApplicationController
+  def index
+    user_states = UserStateFacade.new.get_states(params[:user_id])
+    render json: StateSerializer.new(user_states)
+  end
+
+  def create
+    u_state = UserState.find_by(user_id: params[:user_id], state_id: params[:state_id])
+
+    if u_state
+      u_state.update!(user_id: params[:user_id], state_id: params[:state_id])
+      render json: UserStateSerializer.new(u_state)
+    else
+      new_user_state = UserState.create(user_id: params[:user_id], state_id: params[:state_id])
+      render json: UserStateSerializer.new(new_user_state), status: 201
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,9 @@ Rails.application.routes.draw do
       get '/user_bills', to: 'user_bills#index'
       post '/user_bills', to: 'user_bills#create'
       delete '/user_bills', to: 'user_bills#destroy'
+
+      get '/user_states', to: 'user_states#index'
+      post '/user_states', to: 'user_states#create'
     end
   end
 end
