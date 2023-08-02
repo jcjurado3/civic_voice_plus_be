1cb02e068219384ed0afdf36d4de46a15c57ff99
Ashley Plasket <ashley.plasket@gmail.com>
2023-08-02 12:16:16 -0400

    add states csv, update schema, add rake task

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_01_020022) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_02_161504) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,9 +37,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_01_020022) do
   end
 
   create_table "states", force: :cascade do |t|
-    t.string "abbv_name"
+    t.string "state_abbr"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "state_name"
   end
 
   create_table "user_bills", force: :cascade do |t|
