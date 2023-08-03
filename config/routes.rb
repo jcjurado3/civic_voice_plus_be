bf046f9b0a914c5d36d9d4ef8578a4e9c5ee7e0b
Jimmy Jurado <jurado140@gmail.com>
2023-08-03 11:49:12 -0400

    Adds root path for render

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
+  root "/"
+
   namespace :api do
     namespace :v1 do
       get "/bills", to: "bills#index"
