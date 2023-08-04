daf2c89e8589fb11f6d7da7f10e02a6019358c1d
Ashley Plasket <ashley.plasket@gmail.com>
2023-08-04 16:48:21 -0400

    remove conditional from bill controller

--- app/controllers/api/v1/bills_controller.rb
+++ app/controllers/api/v1/bills_controller.rb
@@ -1,12 +1,7 @@
 class Api::V1::BillsController < ApplicationController
   def index
     fetch_bills = BillSearchFacade.new(params).bills
-
-    if fetch_bills
-      render json: ApiBillSerializer.new(fetch_bills)
-    # else
-    #   render json: {data: { no bills found}}
-    end
+    render json: ApiBillSerializer.new(fetch_bills)
   end
 
   def show
