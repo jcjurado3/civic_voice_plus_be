79200e841f9a7817b55d3a3dbceb83a6f8fb536e
Julian Beldotti <julian.beldotti@gmail.com>
2023-08-04 12:02:49 -0600

    paginate fxn

--- app/controllers/api/v1/bills_controller.rb
+++ app/controllers/api/v1/bills_controller.rb
@@ -1,7 +1,18 @@
 class Api::V1::BillsController < ApplicationController
   def index
-    fetch_bills = BillSearchFacade.new(params).bills
-    render json: ApiBillSerializer.new(fetch_bills)
+    fetch_bills = BillSearchFacade.new(params).bills#.paginate(:page => params[:page])
+    if !Bill.present?
+      render json: ApiBillSerializer.new(fetch_bills).paginate(:page => params[:page])
+    else
+      @bills = Bill.paginate(:page => params[:page])
+      # @bills = ApiBillSerializer.new(Bill.where("state LIKE ?", "%#{params[:state]}%").page(params[:page]))
+      render json: 
+            {
+              bills: @bills,
+              page: @bills.current_page,
+              pages: @bills.total_pages
+            }
+    end
   end
 
   def show
--- app/models/bill.rb
+++ app/models/bill.rb
@@ -2,4 +2,6 @@ class Bill < ApplicationRecord
   has_many :user_bills, dependent: :destroy 
   serialize :sponsors, Array
   serialize :texts, Array
+
+  self.per_page = 8
 end
